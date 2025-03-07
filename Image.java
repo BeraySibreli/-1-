ccb24bd99edf5c0ce8e3cabd9d7f72fbbe9a0ec3
BeraySibreli <beraysibreli69@gmail.com>
2025-03-07 10:10:15 +0200

    Class Image

---
+++ Image.java
@@ -0,0 +1,11 @@
+public class Image {
+    String fileName;
+
+    public Image(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+}
